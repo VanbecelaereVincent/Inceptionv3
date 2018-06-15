e2d7b439437f3cdd59bcc2c0387700aa1be2735c
Vincent Vanbecelaere <vincent.vanbecelaere@student.howest.be>
2018-06-15 12:08:59 +0200

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['tensorflow>=1.4.1', 'keras']
+REQUIRED_PACKAGES = ['tensorflow', 'keras']
 
 setup(
     name='trainer',
